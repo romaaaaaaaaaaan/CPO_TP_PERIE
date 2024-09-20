7286a73156cb60e26b923da2e055231034899cc3
Roman <romanperie04@gmail.com>
2024-09-20 11:17:29 +0200

    Création d'un nouveau projet versionné

---
+++ TP0Exo1/src/tp0exo1/TP0Exo1.java
@@ -0,0 +1,30 @@
+/* TP0 Ex1
+20/09/2024
+*/
+package tp0exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author roman
+ */
+public class TP0Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        
+        System.out.println("Au revoir");
+        
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, uel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
