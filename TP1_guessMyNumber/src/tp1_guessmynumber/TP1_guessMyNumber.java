da1e1d376165d2082ed6c44378071354be34fe74
Roman <romanperie04@gmail.com>
2024-10-07 23:34:56 +0200

    Création d'un nouveau projet versionné

--- TP1_guessMyNumber/src/tp1_guessmynumber/TP1_guessMyNumber.java
+++ TP1_guessMyNumber/src/tp1_guessmynumber/TP1_guessMyNumber.java
@@ -18,33 +18,46 @@ public class TP1_guessMyNumber {
      */
     public static void main(String[] args) {
         Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(100);
-        int cpt =  1;
+        int n = 0;
+        int b ;
+        Scanner sc1;
+        sc1 = new Scanner(System.in);
+        System.out.println("Saisissez un niveau de difficulté :\n"
+                + " 1) Facile : trouver un nombre entre 0 et 10 \n"
+                + " 2) Normal : trouver un nombre entre 0 et 100\n"
+                + " 3) Difficile : trouver un nombre entre 0 et 1000 ");
+        b = sc1.nextInt();
         
+        if( b==1) {
+        n = generateurAleat.nextInt(10);
+        } else if ( b==2 ) {
+        n = generateurAleat.nextInt(100);
+        } else if ( b==3) {
+        n = generateurAleat.nextInt(1000);    
+        }
+        int cpt =  1;
         int a;
         Scanner sc;
         sc = new Scanner(System.in);
-        System.out.println("Saisissez un nombre entre 1 et 100");        
+        System.out.println("Saisissez un nombre ");        
         a = sc.nextInt();
         while (a != n)
-        {
-        
-                    
-        
+        {                            
         if ( a<n)
            {
                System.out.println("C'est plus !");
-               System.out.println("Saisissez un nombre entre 1 et 100");
+               System.out.println("Saisissez un nombre ");
            }else if(a>n)
            {
                System.out.println("C'est moins !");
-               System.out.println("Saisissez un nombre entre 1 et 100");
+               System.out.println("Saisissez un nombre ");
            }
         sc = new Scanner(System.in);
         a = sc.nextInt();
         cpt+=1;
         System.out.println("Nombres de tentatives : "+cpt);
         }
+        System.out.println("Bravo !");
         }
         
     
